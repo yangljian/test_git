6532500d92e148fc85e3f3512f6df9f85a923bab
ylj <ylj@qq.com>
2019-05-04 21:16:06 +0800

    second edit by hot_fix

--- src/main/java/com/atguigu/git/TestGit.java
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -4,6 +4,6 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("test right...");
 		System.out.println("test left...!!!");
-		System.out.println("test hot_fix...!!!");
+		System.out.println("test hot_fix222...!!!");
 	}
 }
